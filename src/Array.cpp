9c11a8338e3036277a9a5fab24e34e156dde8558
chuyenhoang <chuyenhoang.2011@gmail.com>
2020-12-27 18:38:49 +0700

    bổ sung hàm nhập

--- src/Array.cpp
+++ src/Array.cpp
@@ -1,5 +1,17 @@
 #include<stdio.h>
+void enter(int a[], int &n);
 int main(){
     int a[100], n;
+    enter(a,n);
     return 0;
+}
+void enter(int a[], int &n){
+    int temp;
+    printf("n: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("a[%d]: ",i);
+        scanf("%d",&temp);
+        a[i] = temp;
+    }
 }
